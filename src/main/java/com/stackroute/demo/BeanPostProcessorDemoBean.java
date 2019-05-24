7ade9fa868cdfa08f58da392c075f3ef395e6396
maria <mariaaquil09@gmail.com>
2019-05-24 18:00:18 +0530

    Task 6 completed.

---
+++ src/main/java/com/stackroute/demo/BeanPostProcessorDemoBean.java
@@ -0,0 +1,18 @@
+package com.stackroute.demo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class BeanPostProcessorDemoBean implements BeanPostProcessor{
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Called postProcessBeforeInitialization() for :" + beanName);
+        return null;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Called postProcessAfterInitialization() for :" + beanName);
+        return null;
+    }
+}
